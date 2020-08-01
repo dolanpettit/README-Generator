4d462b24479cba9f12da5f7d1643a18563897ced
Dolan Pettit <dolanpettit@gmail.com>
2020-07-31 23:25:29 -0400

    Updating the generateMD file with the README template

---
+++ assets/generateMD.js
@@ -0,0 +1,59 @@
+// function to generate markdown for README
+function generateMarkdown(answers) {
+  return `
+
+  [![made-with-Markdown](https://img.shields.io/badge/Made%20with-Markdown-1f425f.svg)](http://commonmark.org) 
+
+  ${badges[answers.License]}
+
+  
+  # ${answers.Title}
+  
+  
+  
+  
+  
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation instructions](#instillation)
+  * [Usage information](#usage)
+  * [License](#license)
+  * [Contributors](#contributors)
+  * [Test](#test)
+  * [Questions](#questions)
+
+
+  ## Description
+  ${answers.Description}
+
+
+  ## Installation instructions
+  ${answers.Installation}
+  
+  ## Usage information
+  ${answers.Usage}
+  
+
+  ## License
+  ${answers.License}  
+  
+  ## Contributors
+  ${answers.Contributors}
+
+  ## Test
+  ${answers.Test}
+  
+  ## Questions
+
+  For any questions about the project, please feel free to reach out to me on github or via email.  Thank you for viewing this project!
+
+  Contact me: 
+
+  Github:[${answers.Username}]()
+  
+  Email:[${answers.Email}]()
+
+  `;
+}
+
+module.exports = generateMarkdown;
